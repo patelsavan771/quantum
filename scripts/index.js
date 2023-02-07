aec8a5fc7e62372ed10562e7fb44aae0be734f89
Savan Patel <84454428+patelsavan771@users.noreply.github.com>
2023-02-07 14:58:20 +0530

    JSONtask completed with TypeScript (#1)
    * render data using JS/JSON
    
    * replace JS file with JSON file, for data storing
    
    * convert JavaScript written for rendering to TypeScript
    
    * convert JavaScript written for rendering to TypeScript

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+import { generateCard } from "./utils.js";
+const courseContainer = document.querySelector('.course-container');
+const courseCount = document.querySelector('#course-count');
+fetch('/scripts/courses.json')
+    .then((response) => response.json())
+    .then((data) => {
+    courseCount.innerHTML = data.length.toString();
+    data.map((course) => {
+        const card = document.createElement("div");
+        card.classList.add('course-card');
+        card.innerHTML = generateCard(course);
+        courseContainer.appendChild(card);
+    });
+});

c99d8f82381c55e9f25be9c70f52346e9d91b658
Savan Patel <patelsavan771@gmail.com>
2023-02-08 16:28:15 +0530

    complete hover effect on alert, announcement and hamburger-menu

---
+++ scripts/hoverFunc.js
@@ -0,0 +1,55 @@
+const hamburgerContainer = document.querySelector('#hamburger-menu-div');
+const hamburgerIcon = document.querySelector('#hamburger-icon');
+
+hamburgerContainer.addEventListener('mouseover', () => {
+    hamburgerIcon.setAttribute('src', './quantum-screen-assets/icons/hamburger-menuActive.svg');
+});
+hamburgerContainer.addEventListener('mouseout', () => {
+    hamburgerIcon.setAttribute('src', './quantum-screen-assets/icons/hamburger-menu.svg');
+});
+
+let fixedNavItems = document.getElementsByClassName("fixed-nav-item");
+// console.log(fixedNavItems[0].children[0]);
+
+for(let i = 0; i < fixedNavItems.length - 1; i++) {
+    fixedNavItems[i].addEventListener('mouseover', function() {
+        let img = this.children[0].children[0];
+        let span = this.children[0].children[1];
+        let imgSrc = img.getAttribute('src').replace('.svg','Active.svg');
+        img.setAttribute('src', imgSrc);
+        span.style.opacity = 0;
+    });
+
+    fixedNavItems[i].addEventListener('mouseout', function() {
+        let img = this.children[0].children[0];
+        let span = this.children[0].children[1];
+        let imgSrc = img.getAttribute('src').replace('Active.svg','.svg');
+        img.setAttribute('src', imgSrc);
+        span.style.opacity = 1;
+    });
+}
+
+const appendSpan = (ele) => {
+    let dropdownIcon = document.createElement('span');
+    dropdownIcon.classList.add('material-icons','dropdown-btn-icon');
+    dropdownIcon.innerHTML = `&#xE5CF;`;
+    ele.appendChild(dropdownIcon);
+}
+
+let dropdownBtns = document.getElementsByClassName("dropdown-btn");
+
+for (let i = 0; i < dropdownBtns.length; i++) {
+    appendSpan(dropdownBtns[i]);
+    dropdownBtns[i].addEventListener("click", function () {
+        let dropdownContent = this.nextElementSibling;
+        // console.log(this.childNodes[1].innerHTML);
+
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+            this.childNodes[1].innerHTML = '&#xE5CF;';
+        } else {
+            dropdownContent.style.display = "block";
+            this.childNodes[1].innerHTML = '&#xE5CE;';
+        }
+    });
+}

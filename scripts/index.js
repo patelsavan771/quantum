4767c71655bf652d90144fac71eab0cbe8e22d1d
Savan Patel <patelsavan771@gmail.com>
2023-02-08 14:42:55 +0530

    make working hamburger menu on hover

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,26 @@
+let dropdownBtns = document.getElementsByClassName("dropdown-btn");
+
+let i;
+for (i = 0; i < dropdownBtns.length; i++) {
+    let dropdownIcon = document.createElement('span');
+    dropdownIcon.classList.add('material-icons');
+    dropdownIcon.classList.add('dropdown-btn-icon');
+    dropdownIcon.innerHTML = `&#xE5CF;`;
+    dropdownBtns[i].appendChild(dropdownIcon);
+    dropdownBtns[i].addEventListener("click", function () {
+        let dropdownContent = this.nextElementSibling;
+        // console.log(this.childNodes[1].innerHTML);
+
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+            this.childNodes[1].innerHTML = '&#xE5CF;';
+        } else {
+            dropdownContent.style.display = "block";
+            this.childNodes[1].innerHTML = '&#xE5CE;';
+        }
+    });
+}
+
 import { generateCard } from "./utils.js";
 const courseContainer = document.querySelector('.course-container');
 const courseCount = document.querySelector('#course-count');

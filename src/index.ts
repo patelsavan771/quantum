f41be7b2a4266a3f77815823a69ab28cc237e2e1
Savan Patel <patelsavan771@gmail.com>
2023-02-13 11:54:48 +0530

    fix hamburger arrow icon and animation(all done)

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import { Course } from './interfaces.js';
 import { generateCard } from "./utils.js";
 const courseContainer :HTMLElement = document.querySelector('.course-container')!;
 const courseCount: HTMLElement = document.querySelector('#course-count')!;
+const showCourseCount: HTMLElement = document.querySelector('#showing-course-count')!;
 
 
 fetch('/scripts/courses.json')
     .then((response: Response) => response.json())
     .then((data: Course[]): void => {
         //update course-count in nav-toggle
-        courseCount.innerHTML = data.length.toString();
+        let totalCourse = data.length.toString();
+        courseCount.innerHTML = totalCourse;
+        showCourseCount.innerHTML = ` showing ${totalCourse} of ${totalCourse} `;
         
         data.map((course: Course) => {
             const card: HTMLElement = document.createElement("div");
